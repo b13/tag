d89dd8bb96d176d9610ad42e7ee0b54148dc75ec
Benni Mack <benni@typo3.org>
2019-12-30 15:44:42 +0100

    [BUGFIX] Avoid remvoed sys_tag records to be shown

--- Classes/Form/TagListElement.php
+++ Classes/Form/TagListElement.php
@@ -69,6 +69,9 @@ class TagListElement extends AbstractFormElement
         $items = [];
         foreach ($selectedItems as $itemValue) {
             $tagRecord = BackendUtility::getRecord('sys_tag', $itemValue);
+            if (!is_array($tagRecord)) {
+                continue;
+            }
             $items[] = [
                 'value' => (int)$itemValue,
                 'name' => htmlspecialchars($tagRecord['name']),
