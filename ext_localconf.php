fa7b5f71579269e4a66069c267b44c5078f571de
Michael Semle <michael.semle@b13.com>
2025-01-21 14:27:35 +0100

    [TASK] Fix priority setting

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@ defined('TYPO3') || die('Access denied.');
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1433089350] = [
     'nodeName' => 'tagList',
-    'priority' => 60,
+    'priority' => 40,
     'class' => \B13\Tag\Form\TagListElement::class,
 ];
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['b13/tag'] = \B13\Tag\Persistence\PrepareTagItems::class;
